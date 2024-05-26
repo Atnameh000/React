409cd4737133dc7efd1cd519d3ed097c74bc0e28
Atnameh <hemantaneupane76@gmail.com>
2024-05-26 22:59:09 +0545

    learning the reseting way with state

--- reseting_state/src/App.jsx
+++ reseting_state/src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Display from './problem1/problem1'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+       <Display />
     </>
   )
 }
