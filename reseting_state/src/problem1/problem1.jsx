c6f50ebd1abb21add348763a9a9f37753e142133
Atnameh <hemantaneupane76@gmail.com>
2024-05-28 23:30:46 +0545

    code correction

--- reseting_state/src/problem1/problem1.jsx
+++ reseting_state/src/problem1/problem1.jsx
@@ -27,6 +27,10 @@ function Comp(){
     const [text, setText] = useState('')
     return(
         <div>
+          <textarea 
+             value={text}
+             onChange={e => setText(e.target.value)}
+           />
            <textarea 
              value={text}
              onChange={e => setText(e.target.value)}
