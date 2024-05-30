ccf9ace92738324454e5f9ca31ac9167db6a83b2
Atnameh <hemantaneupane76@gmail.com>
2024-05-30 23:13:54 +0545

    code correction

--- reseting_state/src/problem1/problem1.jsx
+++ reseting_state/src/problem1/problem1.jsx
@@ -7,6 +7,9 @@ export default function Display(){
             <div>
                <h1>Hint: My name is Hemanta</h1>
                <Comp key='comp'/>
+               <button
+                 onClick={e => setIsclick(!isclick)}
+               >hide hint</button>
             </div>
         );
     }
